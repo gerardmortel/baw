db29ea303ca754a90af62be738afa3ce5f83776b
gerardmortel <gerard.mortel@gmail.com>
2019-03-06 08:59:17 -0600

    Update install_chefserver.sh

--- install_chefserver.sh
+++ install_chefserver.sh
@@ -6,9 +6,9 @@ wget -nv -P ~/downloads https://packages.chef.io/files/stable/chef-server/12.19.
 wget -nv -P ~/downloads https://packages.chef.io/files/stable/chefdk/3.7.23/ubuntu/18.04/chefdk_3.7.23-1_amd64.deb
 dpkg -i ~/downloads/chef-server-core_12.19.26-1_amd64.deb
 dpkg -i ~/downloads/chefdk_3.7.23-1_amd64.deb
-#chef-server-ctl reconfigure >> ~/chef_server_reconfigure1.log
-#chef-server-ctl cleanse >> ~/chef_cleanse.log
-chef-server-ctl reconfigure >> ~/chef_server_reconfigure2.log
+(chef-server-ctl reconfigure >> ~/chef_server_reconfigure1.log || : )
+(chef-server-ctl cleanse >> ~/chef_cleanse.log || : )
+(chef-server-ctl reconfigure >> ~/chef_server_reconfigure2.log || : )
 
 until (curl -D - http://localhost:8000/_status) | grep "200 OK"; do sleep 15s; done
 while (curl http://localhost:8000/_status) | grep "fail"; do sleep 15s; done
@@ -16,5 +16,5 @@ while (curl http://localhost:8000/_status) | grep "fail"; do sleep 15s; done
 chef-server-ctl user-create gmortel Gerard Mortel gmortel@us.ibm.com 'abc123' --filename ~/drop/gmortel.pem
 chef-server-ctl org-create ibmodc 'IBM On Demand Consulting' --association_user gmortel --filename ~/drop/ibmodc.pem
 chef-server-ctl install chef-manage
-chef-server-ctl reconfigure
-chef-manage-ctl reconfigure
+(chef-server-ctl reconfigure >> ~/chef_server_reconfigure3.log || : )
+(chef-manage-ctl reconfigure >> ~/chef_manage_reconfigure.log || : )
